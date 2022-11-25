c3b872a55ba038507e81ddf231b50165bc304708
Matheuspompeo <matheuspompeo05@hotmail.com>
2022-11-24 21:29:58 -0300

    Add files via upload

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/ubuntu
-#sudo apt-get update
-#sudo apt-get install -y python2.7 python-pip
-pip2 install Flask
-pip install -r requirements.txt
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip
+pip3 install Flask
+pip3 install -r requirements.txt
